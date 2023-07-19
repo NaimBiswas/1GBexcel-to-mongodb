8772c72c64ce564cd6948f61763f77f5f621b1bf
NaimBiswas <nayeembiswas2@gmail.com>
2023-07-20 01:03:54 +0600

    feature:this program is now able to import 1GB excel file into monogodb

--- go.go
+++ go.go
@@ -1,7 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"log"
+	"time"
+
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 type User struct {
 	Id string `json:"id"`
@@ -11,7 +23,117 @@ type User struct {
 }
 
 func main() {
-	fmt.Println("Welcome to google golang.org")
+	fmt.Println("Welcome to xlsx program! :: Naim Biswas")
+	importXLSX()
+}
+
+func importXLSX()  {
+
+	
+	xlsxFilePath, _ := filepath.Abs("./file/employeeSheets.xlsx")
+	xlsxFile, err := excelize.OpenFile(xlsxFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	db := dbConnection()
+
+	startTime := time.Now()
+	for _, sheetName  := range xlsxFile.GetSheetList() {
+		fmt.Println("sheet Name:",sheetName)
+		timeForEachSheet := time.Now()
+		rows, err := xlsxFile.GetRows(sheetName)
+		// columns, err := xlsxFile.GetCols(sheetName)
+
+		if err != nil {
+			log.Println("Error reading sheet rows:", err)
+			continue
+		}
+		colNames  := rows[0]
+		rowValues := rows[1:]
+		
+		var jsonData []map[string]interface{}
+		fmt.Printf("Xlsx Process Started for=================================================================::%v and time:: %v",sheetName, time.Now())
+		fmt.Println()
+		for _, rowValue := range rowValues {
+			data :=  make(map[string]interface{})
+
+			for idx, rV := range rowValue {
+				if colNames[idx] != "" {
+					data[colNames[idx]] = rV
+				 }
+			}
+			jsonData = append(jsonData, data)
+		}
+		insertRecords(db, "excelData", jsonData)
+		jsonBytes, err := json.MarshalIndent(jsonData, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		outputFile, err := os.Create("./convertedFile/"+sheetName+".json")
+		defer outputFile.Close()
+		
+		// Write JSON data to a file
+		_, err = outputFile.Write(jsonBytes)
+		fmt.Println("Conversion completed. JSON data is stored in",sheetName+".json", time.Since(timeForEachSheet))
+	}
+	elapsedTime := time.Since(startTime)
+	// Print the elapsed time
+	fmt.Println("Insertion completed in:", elapsedTime)
+}
 
 
+func insertRecords(db *mongo.Database, collectionName string, data []map[string]interface{}) error {
+	collection := db.Collection(collectionName)
+	var documents []interface{}
+	for _, record := range data {
+		if len(record) != 0 {
+			documents = append(documents, record)
+		} 
+	}
+	_, err := collection.InsertMany(context.Background(), documents)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println("Records Inserted Into Database,collection:", collectionName, "RecordLength:", len(documents))
+	return nil
+}
+
+func getAllData(db *mongo.Database, collectionName string) ([]interface{})   {
+	
+	cur, err := db.Collection(collectionName).Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		var data []interface{}
+		err := cur.Decode(&data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return data
+	}
+	return []interface{}{}
+}
+
+func dbConnection() (*mongo.Database) {
+	mongoURL := "mongodb+srv://NaimBiswas:yUhrDIMBoOAPKV2J@ecom.9wdffud.mongodb.net/eCom?retryWrites=true&w=majority"
+	// collectionName := "excelData"
+	dbName := "eCom"
+
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("error:",err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database(dbName)
+	return db
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,29 @@ module Golang/go
 
 go 1.20
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0
+	rsc.io/quote v1.5.2
+)
 
 require (
-	golang.org/x/text v0.10.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/tealeg/xlsx v1.0.5 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/xuri/efp v0.0.0-20230422071738-01f4e37c47e9 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	rsc.io/sampler v1.99.99 // indirect
 )
